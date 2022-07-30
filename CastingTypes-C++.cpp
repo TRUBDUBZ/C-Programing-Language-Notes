819a4ef5f715753e9d933e64025dbd6a932045e5
trub <marcusdiedrich1@icloud.com>
2022-07-29 19:27:32 -0600

    Update CastingTypes-C++.cpp

--- CastingTypes-C++.cpp
+++ CastingTypes-C++.cpp
@@ -1,14 +1,14 @@
 # Casting Types
 
 - Sometimes you need to change the *type* of a *variable*.
-- for instance you may need to change from a variable type int to a     variable type of double
+- for instance you may need to change from a variable type int to a variable type of double
 - this would not work:
 
-`
+
 int a;
 double b = 3.0;
   a = b;
-`
+
 
 - There are two types of conversions, *implicit* and *explicit*. When you set one variable to be the same as another, such as a = b above, but their types do not match, the compiler will try to convert them. However, this can be dangerous as you might not know what values will be in the variable at runtime, and it might not be implicitly convertible to another type. Our example above was *implicit*, the compiler in this case will make a best guess.
 
@@ -18,13 +18,13 @@ double b = 3.0;
 
 - A more interesting casting option is converting a char to a number type, or the other way around. Just like before, you have to be careful how you set this up. In the back-end, a char doesn’t store 'a', it stores the value representing that: 97 for lowercase and 65 for uppercase.
 
-`int targetInt;`
-`char sourceChar = 'a';`
-`targetInt = (int)sourceChar;`
+int targetInt;
+char sourceChar = 'a';
+targetInt = (int)sourceChar;
 
 - Now `targetInt` equals 97.
 - When you cast an `int` to a `char`, you get the opposiste process and the char is set to the value at the `int` value. So if you did:
 
-`int sourceInt = 65;`
-`char targetChar;`
-`targetChar = (char)sourceInt;`
+int sourceInt = 65;
+char targetChar;
+targetChar = (char)sourceInt;

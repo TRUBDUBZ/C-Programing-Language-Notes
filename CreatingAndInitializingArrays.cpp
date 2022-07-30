2aabae63d8a32a6c8e02f2c6f05bdb1d7f9a5360
trub <marcusdiedrich1@icloud.com>
2022-07-29 19:30:05 -0600

    Update and rename CreatingAndInitializingArrays.c to CreatingAndInitializingArrays.cpp

--- CreatingAndInitializingArrays.cpp
+++ CreatingAndInitializingArrays.cpp
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
- // Creating And Initializing Arrays
-=======
 // Creating And Initializing Arrays
->>>>>>> 5cbaa9480fd828b374b90e2e6f60525ba8f6bcae
-
 
 // An array is a collection of data that can be any type. For example, you can construct an array of 'int's, 'bool's, 'char's, etc.
 
@@ -15,29 +10,27 @@ int age[4];
 
 // To indicate to the compiler that age is an array of ints and not a single int variable, we append [arraySize] (in this case, arraySize is four) to the end of the variable name.
 // The general template for creating an uninitialized array is 
-<<<<<<< HEAD
-`dataType name[arraySize];`
-=======
-  dataType name[arraySize];
->>>>>>> 5cbaa9480fd828b374b90e2e6f60525ba8f6bcae
+dataType name[arraySize];
+
+dataType name[arraySize];
 
 // When you create an uninitialized array, you are required to specify its size so that the compiler may reserve the proper amount of memory blocks. Once it is created, the size of the array cannot be changed; this means that arrays are static. An uninitialized array can be populated later in a program.
 // In contrast, an initialized array is created by specifying the initial value of its elements. As an example, we will create an age array with four initial ages:
+
 int age[] = {7, 27, 34, 63};
 
 // The general template for creating an initialized array is:
 
-<<<<<<< HEAD
-`dataType name[] = {firstValue, secondValue, thirdValue, …};`
-
-// Notice that in this case, you don’t need to specify the size of the array as it is implied from the number of elements supplied; however, declaring an initialized array with the size specified will also work. The previous example could have also been written like this:
-
-`int age[4] = {7, 27 34, 63};`
-=======
 dataType name[] = {firstValue, secondValue, thirdValue, …};
 
 // Notice that in this case, you don’t need to specify the size of the array as it is implied from the number of elements supplied; however, declaring an initialized array with the size specified will also work. The previous example could have also been written like this:
 
 int age[4] = {7, 27 34, 63};
->>>>>>> 5cbaa9480fd828b374b90e2e6f60525ba8f6bcae
+
+dataType name[] = {firstValue, secondValue, thirdValue, …};
+
+// Notice that in this case, you don’t need to specify the size of the array as it is implied from the number of elements supplied; however, declaring an initialized array with the size specified will also work. The previous example could have also been written like this:
+
+int age[4] = {7, 27 34, 63};
+
 
